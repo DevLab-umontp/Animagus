6fc633045c184668de5218c6f562d54440d5cd50
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-03-26 12:12:11 +0100

    Ajout : UploadImageTest

---
+++ src/test/java/fr/umontpellier/iut/UploadImageTest.java
@@ -0,0 +1,14 @@
+package fr.umontpellier.iut;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UploadImageTest {
+
+    @Test
+    void test_getLien_eguale_bon_lien() {
+        String resultatAttendue = "https://github.com/MathieuSoysal/Animagus.umontp/upload/master/images/animaux/test";
+        assertEquals(resultatAttendue, UploadImage.getLink("&add test"));
+    }
+}
