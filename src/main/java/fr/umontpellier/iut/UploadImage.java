1fb3877e6ab3b644f3235b3226ef69496f7c6ac5
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-03-27 16:57:33 +0100

    Correction : syntaxique

--- src/main/java/fr/umontpellier/iut/UploadImage.java
+++ src/main/java/fr/umontpellier/iut/UploadImage.java
@@ -3,7 +3,7 @@ package fr.umontpellier.iut;
 import java.util.Random;
 
 public class UploadImage {
-    static final String[] ICONS_ANIMAUX = { "🐵", "🐶", "🐺", "🐱", "🦁", "🐯", "🦒", "🦊", "🦝", "🐮", "🐷", "🐗",
+    static final String[] ICONES_ANIMAUX = { "🐵", "🐶", "🐺", "🐱", "🦁", "🐯", "🦒", "🦊", "🦝", "🐮", "🐷", "🐗",
             "🐭", "🐹", "🐰", "🐻", "🐨", "🐼", "🐸", "🦓", "🐴", "🦄", "🐔", "🦧", "🦮", "🐕‍🦺", "🐩", "🐕", "🐈",
             "🐅", "🐆", "🐎", "🦌", "🦏", "🦛", "🐂", "🐃", "🐄", "🐖", "🐏", "🐑", "🐐", "🐪", "🐫", "🦙", "🦘", "🦥",
             "🦨", "🦡", "🐘", "🐁", "🐀", "🦔", "🐇", "🐿", "🦎", "🐊", "🐢", "🐍", "🦕", "🦖", "🦦", "🦈", "🐬", "🐳",
@@ -13,11 +13,11 @@ public class UploadImage {
     public static String getLien(String messageRecue) {
         String animalChoisi = messageRecue.substring(5).toLowerCase();
         return String.format(
-                "Clic sur le lien pour ajouter ton image de %s marrant favori %s  %s  %s  :\n 📎 https://github.com/MathieuSoysal/Animagus.umontp/upload/master/images/animaux/%s \n",
+                "Clic sur le lien pour ajouter ton image de %s marrant %s  %s  %s  :\n 📎 https://github.com/MathieuSoysal/Animagus.umontp/upload/master/images/animaux/%s \n",
                 animalChoisi, getRandomIcone(), getRandomIcone(), getRandomIcone(), animalChoisi);
     }
 
     private static String getRandomIcone() {
-        return ICONS_ANIMAUX[new Random().nextInt(ICONS_ANIMAUX.length)];
+        return ICONES_ANIMAUX[new Random().nextInt(ICONES_ANIMAUX.length)];
     }
 }
