e7ecff5c5e084dc64a520fed13c89cfc158742be
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-04-12 15:55:06 +0200

    Mise à jour : comande help

--- src/main/java/fr/umontpellier/iut/commandes/Help.java
+++ src/main/java/fr/umontpellier/iut/commandes/Help.java
@@ -14,6 +14,7 @@ class Help extends ListenerAdapter implements Commande {
         final String ANIMAL = "\n ► `&{votre animal}` : par exemple `&chien` pour afficher l'image d'un chien.";
         final String ADD = "\n ► `&add {votre animal}` : par exemple `&add souris` pour ajouter l'image d'une souris.";
         final String FEEDBACK = "\n ► `&feedback` : permet de faire un retour sur le bot.";
-        messageRecue.getChannel().sendMessage(AIDE + ANIMAL + ADD + FEEDBACK).queue();
+        final String INFO = "\n\n Pour plus d'info rendez-vous sur https://github.com/MathieuSoysal/Animagus.umontp";
+        messageRecue.getChannel().sendMessage(AIDE + ANIMAL + ADD + FEEDBACK + INFO).queue();
     }
 }
