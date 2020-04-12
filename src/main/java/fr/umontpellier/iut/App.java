e218ec376072836f6dd3c9a547800dac01cd79c7
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-04-12 15:47:22 +0200

    Mise à jour : App : Authenfication discord bot

--- src/main/java/fr/umontpellier/iut/App.java
+++ src/main/java/fr/umontpellier/iut/App.java
@@ -9,16 +9,22 @@ public class App {
      *
      */
     private static final JDABuilder ANIMAGUS_BUILDER = new JDABuilder(AccountType.BOT)
-            .setToken("NjkyMzk3MTM4MjY4NjUxNTYy.XnwTSg.gBlqAYCwYSQJCjsSjSMj18d-xug")
             .addEventListeners(new GestionnaireEvents())
             .setActivity(Activity.streaming("Animagus.umontp",
                     "https://vignette.wikia.nocookie.net/harrypotter/images/3/30/McGonagall_Animagus.gif/revision/latest?cb=20150715164411&path-prefix=fr"))
             .setCompression(Compression.NONE);
 
     public static void main(String[] args) {
-        JDABuilder builder = ANIMAGUS_BUILDER;
+        verifieArguments(args);
+        JDABuilder builder = ANIMAGUS_BUILDER.setToken(args[0]);
         for (int i = 0; i < 10; i++)
             login(builder.useSharding(i, 10));
+    }
+
+    private static void verifieArguments(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Veuillez mettre en argument le token du bot discord");
+          }
     }
 
     private static void login(JDABuilder builder) {
