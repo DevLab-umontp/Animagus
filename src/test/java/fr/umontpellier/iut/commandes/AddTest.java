0d8c5cb1cc26498e29717149e56586d925247e0c
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-09-25 23:59:15 +0200

    Mise à jour : AddTest

--- src/test/java/fr/umontpellier/iut/commandes/AddTest.java
+++ src/test/java/fr/umontpellier/iut/commandes/AddTest.java
@@ -8,7 +8,7 @@ public class AddTest {
 
     @Test
     void test_getLien_eguale_bon_lien() {
-        String resultatAttendue = "https://github.com/MathieuSoysal/Animagus.umontp/upload/master/images/animaux/test";
+        String resultatAttendue = "https://github.com/MathieuSoysal/Animagus.umontp/issues/new?labels=Ajout+image&title=Ajout+image+de+test&body=Glisser+votre+image+ici";
         assertTrue(Add.getLien("&add test").contains(resultatAttendue));
     }
 }
