ed78e0d7ee0a6680d594a99faf8f4b109619c383
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-09-26 00:31:16 +0200

    Validation test

--- src/test/java/fr/umontpellier/iut/commandes/AddTest.java
+++ src/test/java/fr/umontpellier/iut/commandes/AddTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class AddTest {
+class AddTest {
 
     @Test
     void test_getLien_eguale_bon_lien() {
-        String resultatAttendue = "https://github.com/DevLab/Animagus.umontp/issues/new?labels=Ajout+image&title=Ajout+image+de+test&body=Glisser+votre+image+ici";
+        String resultatAttendue = "https://github.com/DevLab-umontp/Animagus.umontp/issues/new?labels=Ajout+image&title=Ajout+image+de+test&body=Glisser+votre+image+ici";
         assertTrue(Add.getLien("&add test").contains(resultatAttendue));
     }
 }
